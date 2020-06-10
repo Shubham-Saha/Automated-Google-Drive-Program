ac717368809295ebf368aae9f6128be9e596267d
Shubham-Saha <53710056+Shubham-Saha@users.noreply.github.com>
2020-06-11 04:10:07 +0600

    Update upload_file_to_DriveRoot Function

--- fileProcess.py
+++ fileProcess.py
@@ -23,14 +23,24 @@ def isfolder_exists(folder_name):
             folder_id = create_new_folder(folder_name)
             return folder_id
 
+uploaded_files_list = {}
+        
 def upload_file_to_DriveRoot(title, path):
     # Create GoogleDriveFile instance
     file = drive.CreateFile({'title': title})
     file.SetContentFile(path + title)
     file.Upload()
+    
+    uploaded_files_list[file['title']] = file['id']
+    os.chdir(path)
+    with open("upload_list.csv", 'w+', newline='') as ufile:
+        writer = csv.writer(ufile)
+        for key, value in set(uploaded_files_list.items()):
+            writer.writerow([key, value])
+            
     return True
 
-uploaded_files_list = {}
+
 
 def upload_file_to_DriveFolder(title, fid, path):
     file = drive.CreateFile({"title": title, "parents": [{"kind": "drive#fileLink", "id": fid}]})
