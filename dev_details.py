590284b14bfd280383dd762c5144cf792c7ddb4a
Shubham-Saha <53710056+Shubham-Saha@users.noreply.github.com>
2020-06-11 01:20:38 +0600

    Add Developer details file
    In this file, Developer's details is mentioned.

---
+++ dev_details.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def dev_details():
+    print("-"*100)
+    print("Developed by: Monarchs Technologies Ltd.")
+    print("Idea Pitched by: Md. Ishtiaq Iqbal, Lecturer & Course Co-ordinator, UITS")
+    print("Program Developed by: Shubham Saha Shuv, Co-Founder & CEO, Monarchs Technologies Ltd.")
+    print("-"*100)
